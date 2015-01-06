2a945d477aa229074211f6b6a0b9ef7c1574710d
shahnikita <shahnikita.1991@yahoo.com>
2015-01-05 16:15:54 -0800

    Added my project

---
+++ JqueryClickApp/Html/scripts/srcipt1.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    $('#success').hide();
+    var score = 0;
+
+
+    $('#clickimg').click(function () {
+        if (score == 0) {
+            $('#startover').hide();
+        }
+        score++;
+        $('#score').text(score);
+        $('#success').show('1000').fadeOut(2000);
+    });
+
+
+})
